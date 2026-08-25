33114d13aca3100ae24b402e4c2989db578e9c9d
Roberto Fichera <roby.fichera@gmail.com>
2026-08-25 20:19:30 +0200

    build: version 1.6.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "it.robertofichera.myshoppinglist"
         minSdk = 31
         targetSdk = 37
-        versionCode = 9
-        versionName = "1.6"
+        versionCode = 10
+        versionName = "1.6.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
